6c699cada4fbb97806d3a03da21940a188edf44d
dojo <djdumaluan.prog@gmail.com>
2026-08-29 05:08:27 +0800

    chore: define & register tanstack query client

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,13 +4,17 @@ import "./styles/index.css";
 import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <TooltipProvider>
-        <App />
-      </TooltipProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <App />
+        </TooltipProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
