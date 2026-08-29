993e5e00a8d283cae1d8a88cadefa5f41ef45eb9
dojo <djdumaluan.prog@gmail.com>
2026-08-29 19:46:56 +0800

    fix: allow credentials + specify origin

--- apps/backend/src/sockets/index.ts
+++ apps/backend/src/sockets/index.ts
@@ -2,10 +2,13 @@ import { Server as HttpServer } from "http";
 import { Server } from "socket.io";
 import socketAuthMiddleware from "./middlewares/socket.auth.middleware";
 import handleConnection from "./connection";
+import ENV from "../config/env";
 
 const initializeSocket = (httpServer: HttpServer) => {
   // SocketIO Server Instance
-  const io = new Server(httpServer, { cors: { origin: "*" } });
+  const io = new Server(httpServer, {
+    cors: { origin: ENV.CLIENT_URL, credentials: true },
+  });
 
   // Socket Auth Middleware
   io.use(socketAuthMiddleware);
